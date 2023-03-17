a608f7e5387d9f7c724219c75a3aef37de79c50a
Hamza Khiar <hamza.khiar.01@gmail.com>
2023-03-17 11:38:10 +0100

    Snailotype Your way to a blazingly fast Typer

--- react-snailotyper/src/Typer/Typer.ts
+++ react-snailotyper/src/Typer/Typer.ts
@@ -99,6 +99,7 @@ class Typer {
           : testLog.chrono
       ),
     };
+    console.log(resultObj);
     return resultObj;
   }
 }
--- react-snailotyper/src/components/ResultComp.tsx
+++ react-snailotyper/src/components/ResultComp.tsx
@@ -8,25 +8,59 @@ export function Result({ testLog }: { testLog: testObj }) {
    */
   const [resultLog, setResultLog] = useState(testLog);
   useEffect(() => {
-    return setResultLog(typer.result(testLog));
+    setResultLog(typer.result(testLog));
   }, [true]);
-
   return (
     <div id="result-test">
       <div id="chart-acc-err">
-        <canvas id="chartDisplay"></canvas>
+        {/* <Doughnut data={data} /> */}
+        <p className="top">{resultLog.accuracy} %</p>
+        <p className="bottom">Accuracy</p>
+        <p className="error Bottom">error : {resultLog.error}</p>
       </div>
       <div id="wpm">
-        <p>{resultLog.wpm}</p>
-        <p>WPM</p>
+        <p className="top">{resultLog.wpm}</p>
+        <p className="bottom">WPM</p>
       </div>
       <div id="timeDisplay">
-        <p>
+        <p className="top">
           {resultLog.testType.type == "time"
-            ? `${resultLog.testType.value}s`
+            ? `${resultLog.testType.value}`
             : `${resultLog.chrono}`}
         </p>
+        <p className="bottom">time</p>
       </div>
     </div>
   );
 }
+/** 
+ * 
+ * <p className="top">{resultLog.accuracy}</p>
+        <p className="bottom">Accuracy</p>
+        <p className="error Bottom">error :{resultLog.error}</p>
+*/
+
+/* 
+// import { Doughnut } from "react-chartjs-2";
+// import { Chart, ArcElement, Tooltip, Legend } from "chart.js";
+
+// Chart.register(ArcElement, Tooltip, Legend);
+
+
+
+  // const data = {
+  //   labels: ["Accuracy in % ", "Errors"],
+  //   datasets: [
+  //     {
+  //       data: [resultLog.accuracy, resultLog.error],
+  //       backgroundColor: [
+  //         "rgba(222, 227, 72, 0.68)",
+  //         "rgba(255, 124, 76, 0.6)",
+  //       ],
+  //       borderWidth: 1,
+  //       hoverOffset: 2,
+  //     },
+  //   ],
+  // };
+
+*/
